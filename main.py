a3eb58876ac227ea132c92bf8c0599ba8c830d72
Leśnik <112889734+tenktopodpalil@users.noreply.github.com>
2024-12-30 17:20:10 +0100

    2

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from os import write
 import os
 a = os.listdir('input files')
 print('%a' % (a))
-for x in a:
-    name = x
-    command = f'ffmpeg -i ".\input files\{name}" -vf scale=1024x576 -b:v 1M ".\output files\output{name}"'
-    os.system(command)
+for name in a:
+    if name.endswith('.foo'):
+        os.remove(f'.\input files\{name}')
+        os.remove(f'.\output files\{name}')
+    else:
+        command = f'ffmpeg -i ".\input files\{name}" -vf scale=1024x576 -b:v 1M ".\output files\output{name}"'
+        os.system(command) 
+
+    
