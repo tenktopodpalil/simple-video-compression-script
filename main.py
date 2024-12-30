8aaf6fbe5be244b6cec7adae94eb0d020fbf8990
Leśnik <112889734+tenktopodpalil@users.noreply.github.com>
2024-12-30 17:06:11 +0100

    init

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from os import write
-
-import PIL
-from PIL import Image
 import os
 a = os.listdir('input files')
 print('%a' % (a))
@@ -9,5 +6,3 @@ for x in a:
     name = x
     command = f'ffmpeg -i ".\input files\{name}" -vf scale=1024x576 -b:v 1M ".\output files\output{name}"'
     os.system(command)
-
-
