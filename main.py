a707fdcf893210efbf2f55a70de20e97de1e8a8d
Leśnik <112889734+tenktopodpalil@users.noreply.github.com>
2025-01-01 22:46:20 +0100

    k

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import os
 a = os.listdir('input files')
 print('%a' % (a))
 for name in a:
-    if name.endswith('.foo'):
+    if name.endswith('.placeholder'):
         os.remove(f'.\input files\{name}')
         os.remove(f'.\output files\{name}')
     else:
         command = f'ffmpeg -i ".\input files\{name}" -vf scale=1024x576 -b:v 1M ".\output files\output{name}"'
         os.system(command) 
+    
 
     
